477dde77511620eb085267df97364468bdba0ae8
douglassmoss <douglassmoss@icloud.com>
2021-12-14 07:34:06 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }
+  else if (someValue >= 2001 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else  (someValue > 2500) 
+    return 'No can do.'
+  
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+let city = 'New York';
+return city ? "Ok, sounds good." : "No go.";
+};
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (switchOnCharmFromTip) {
+    case 'generous': console.log("Thank you so much.");
+    case 'not as generous': console.log("Thank you.");
+    break;
+    default: console.log("Bye");
+  }
 }
